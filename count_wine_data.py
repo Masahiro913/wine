7bacb596ce0fef2668e87cbeb6d21d190d981688
Masahiro913 <gonzaes0819@yahoo.co.jp>
2020-11-23 20:08:13 +0900

    plot count of wine grouped by quality

---
+++ count_wine_data.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#ワインデータの読み込み
+wine = pd.read_csv("winequality-white.csv", sep=",", encoding="utf-8")
+
+#品質データごとにグループ分けをして、その数を数える
+count_data = wine.groupby('quality')["quality"].count()
+print(count_data)
+
+#数えたデータをグラフに描画
+count_data.plot()
+plt.savefig("wine-count-plt.png")
+plt.show()
